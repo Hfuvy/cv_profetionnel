a48cefdcce16ae71ef14fb494e04fb5cccfb5047
Hfuvy <fokoufossojordan@gmail.com>
2024-09-21 16:49:07 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+let menuBtn = document.querySelector('#menu-btn');
+let closeBtnu = document.querySelector('#close-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menuBtn.onclick = () => {
+    navbar.classList.add('active')
+}
+closeBtnu.onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.reviews .box-container .box');
+
+let index = 0;
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active')
+}
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1 ) % slides.length;
+    slides[index].classList.add('active')
+}
